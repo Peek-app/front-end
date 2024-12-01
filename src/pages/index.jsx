7892f3cc9a29c478c74672d2fbd45d32fd506781
Arturo Juarez <ajrobles98@gmail.com>
2024-12-01 15:56:58 -0600

    feat : adding navBar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
 import Branding from "./landing/branding";
+import Navbar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar></Navbar>
       <Branding />
     </main>
   );
