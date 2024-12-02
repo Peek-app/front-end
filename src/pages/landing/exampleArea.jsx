e3da1deab7e70654190624ab0dd46c2ce229afb5
Erick Vera <erickverac90@gmail.com>
2024-12-01 22:10:41 -0600

    Agregando estilos faltantes a area de ejemplo

--- src/pages/landing/exampleArea.jsx
+++ src/pages/landing/exampleArea.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import { MdOutlinePets } from "react-icons/md";
 
 export default function ExampleArea() {
   return (
-    <main className="h-landingExampleArea grid grid-cols-1 py-20 lg:grid-cols-12 lg:gap-8 bg-congress-950">
+    <main className="h-landingExampleArea grid grid-cols-1 py-20 lg:grid-cols-12 lg:gap-8 bg-gradient-landing">
       <section className="lg:col-start-2 lg:col-span-4 flex flex-col justify-between h-full">
         <div>
           <h2 className="text-5xl font-bold text-orange-500">
@@ -13,25 +14,25 @@ export default function ExampleArea() {
         <div className="text-3xl font-bold pr-28">
           <h3>
             Toda la información que necesitas y{" "}
-            <span className="text-orange-500">más</span>
+            <span className="text-congress-300">más</span>
           </h3>
         </div>
         <div className="font-bold pr-28">
-          <h4 className="text-3xl">En un sólo lugar</h4>
-          <p className="text-2xl">
+          <h4 className="text-3xl inline-flex items-center">
+            <MdOutlinePets className="mr-3" />
+            En un sólo lugar
+          </h4>
+          <p className="text-lg">
             Vacunas, historial clínico, alergias, cuidados especiales. etc.
           </p>
         </div>
         <div className="font-bold pr-28">
-          <h4 className="text-3xl">Perfil social</h4>
-          <p className="text-2xl">
+          <h4 className="text-3xl inline-flex items-center">
+            <MdOutlinePets className="mr-3" />
+            Perfil social
+          </h4>
+          <p className="text-lg">
             Muestra a todos a tu compañero, comparte sus logros
-          </p>
-        </div>
-        <div className="font-bold pr-28">
-          <h4 className="text-3xl">Veterinarios</h4>
-          <p className="text-2xl">
-            Encuentra a un veterinario y programa una cita.
           </p>
         </div>
       </section>
