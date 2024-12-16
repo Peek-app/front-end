4cb7134d662a1d766088248ac523c00e2caa47bf
Erick Vera <erickverac90@gmail.com>
2024-12-15 21:50:45 -0600

    Agregando servicio para mostrar todas las mascotas del usuario

---
+++ src/pages/api/services/pets/getAllPets.js
@@ -0,0 +1,30 @@
+const api = process.env.NEXT_PUBLIC_API_URL;
+
+export async function getAllPets() {
+  try {
+    const token = localStorage.getItem("access-token");
+    if (!token) {
+      throw new Error("No access token found");
+    }
+
+    const response = await fetch(`${api}/pets`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      const errorMessage = data.message || "Error fetching pets.";
+      throw new Error(errorMessage);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error getAllPets:", error.message);
+    throw error;
+  }
+}
